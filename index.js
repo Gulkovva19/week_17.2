b206296556f7dab603c3cb1ea7e3e9ea7d327fb8
Gulkovva19 <gulkova.sofya@yandex.ru>
2022-02-17 14:06:12 +0300

    week_18 localStorage without img

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
+// если захочу сохранять имя пользователя в инпуте
+
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     let name = localStorage.getItem('name');
+//     if (name != null) {
+//         document.getElementById("author").value = name;
+//     }
+
+// });
+
 let comments = [];
+document.addEventListener("DOMContentLoaded", function (event) {
+    loadComments();
+});
 
 const checkSpam = document.querySelector('#btn');
 checkSpam.addEventListener("click", () => {
+    event.preventDefault();
+    let author = document.getElementById("author").value;
     let message = document.getElementById("comments").value;
     let newmessage = message.toLowerCase();
 
     if (newmessage.includes('xxx') || newmessage.includes('viagra')) {
-        let n = newmessage.replace("viagra", "***");
-        let m = n.replace("xxx", "***");
-        comments.push(m);
+        let n = newmessage.replace("viagra", "***").replace("xxx", "***");
+        newmessage = n;
     }
 
-    else {
-        comments.push(newmessage);
+    if (localStorage.getItem('name') == null) {
+        localStorage.setItem('name', author);
     }
 
+    let fullcom = {
+        name: author,
+        body: newmessage
+    }
+
+    author = '';
+    newmessage = '';
+    comments.push(fullcom);
+    saveComments();
+    showComments();
     console.log(comments);
-    generateComments();
 })
 
 
-
-function generateComments() {
-    let optionString = "";
-    for (let comment of comments) {
-
-        optionString += `<div class="message" id="mmessage"><span>${comment}</span><button class="comments__button_small id="btndelete"">Удалить</button></div>`;
-    }
-    document.getElementById("commentslist").innerHTML = optionString;
+let saveComments = () => {
+    localStorage.setItem('comments', JSON.stringify(comments));
 }
 
+let loadComments = () => {
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
 
-const commentDel = document.querySelector('#btndelete');
-commentDel.addEventListener("click", () => {
-    let elem = document.getElementById("mmessage");
-    elem.firstChild.remove();
-})
+let showComments = () => {
+    let commentslist = document.getElementById('commentslist');
+    let out = '';
+    comments.forEach(function (item) {
+        out += `<div class="message" id="newmessage">${item.name}:<span>${item.body}</span><button class="comments__button_small" id="btndelete">Удалить</button></div>`
+    });
+    commentslist.innerHTML = out;
+}
